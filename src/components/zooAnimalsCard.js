47b2f64edd40c8d66dc1a3267a27fb0bc2c9cdaa
Clayvis Rawlins <clayraw@amazon.com>
2022-10-07 12:28:19 -0700

    Init Commit

---
+++ src/components/zooAnimalsCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+
+const ZooAnimalCard = (props) => {
+	const { name, photo } = props;
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Img variant="top" src={photo} />
+			<Card.Body>
+				<Card.Title>{name}</Card.Title>
+				<Card.Text>
+					Some quick example text to build on the card title and make
+					up the bulk of the card's content.
+				</Card.Text>
+				<Button variant="primary">Go somewhere</Button>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default ZooAnimalCard;
---
+++ src/components/zooAnimalsList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ZooAnimalCard from "./zooAnimalsCard";
+
+const ZooAnimalsList = () => {
+	const [animals, setAnimals] = useState([]);
+
+	useEffect(() => {
+		getZooAnimals()
+			.then((res) => {
+				console.log(res.data);
+				setAnimals(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	const mapZooAnimals = animals.map((aZooAnimal) => {
+		return (
+			<ZooAnimalCard
+				name={aZooAnimal.name}
+				photo={aZooAnimal.image_link}
+			/>
+		);
+	});
+
+	function getZooAnimals() {
+		let config = {
+			method: "GET",
+			url: `https://zoo-animal-api.herokuapp.com/animals/rand/6`,
+		};
+		return axios(config);
+	}
+
+	return <div className="animal-cards">{mapZooAnimals}</div>;
+};
+
+export default ZooAnimalsList;
